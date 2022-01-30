28caa38ce9b3d2e91a096396ce1ae0aafe4abed5
Crowdbotics <team@crowdbotics.com>
2022-01-30 15:48:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/evspn_powerline_33246/MainActivity.java
@@ -0,0 +1,15 @@
+package com.evspn_powerline_33246;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "evspn_powerline_33246";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "evspn_powerline_33246",
+  url: "https://evspn_powerline_33246.botics.co",
+  api: "https://evspn_powerline_33246.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
